9de1875cc9ae509adcd55133e944e4c386f5bda6
sirgid77 <okechukwuifeanyichukwugideon77@gmail.com>
2026-07-15 23:38:08 +0100

    i added a few changes in my lesson1 files

---
+++ lesson1/string_functions.py
@@ -0,0 +1,6 @@
+# 10 string functions in python and how to use them wealthy man 
+# 1:title(): Capitalizes the first letter of every word.
+my_club = "i am a man united fan"
+my_club = my_club.title()
+print(my_club)
+
