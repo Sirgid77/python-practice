f3c132fd9f3eca3dd857ace4c7e33ef7c637a971
sirgid77 <okechukwuifeanyichukwugideon77@gmail.com>
2026-07-16 18:30:04 +0100

    i completed the assighmemt on list_gid.py file about list

--- lesson2/list_gid.py
+++ lesson2/list_gid.py
@@ -1,11 +1,19 @@
-# Your own code here
+
+# Assignment: Using the list below do the following:
 
 long_list_of_cars = ["Toyota", "Honda", "Ford", "Chevrolet", "BMW", "Mercedes", "Audi", 
                      "Volkswagen", "Nissan", "Hyundai", "Kia", "Mazda", "Subaru", "Jeep", 
                      "Dodge", "Ram", "GMC", "Lexus", "Acura", "Infiniti"]
 
-# Assignment: Using the list above do the following:
 # 1. Print the first car in the list.
 # 2. Print the last car in the list.
 # 3. Print the first five cars in the list.
 # 4. Print the last five cars in the list.
+
+print(long_list_of_cars[0])  # Output: Toyota
+
+print(long_list_of_cars[-1])  # Output: Infiniti
+
+print(long_list_of_cars[0:5])  # Output: ['Toyota', 'Honda', 'Ford', 'Chevrolet', 'BMW']
+
+print(long_list_of_cars[-5:])  # Output: ['Dodge', 'Ram', 'GMC', 'Lexus', 'Acura']
